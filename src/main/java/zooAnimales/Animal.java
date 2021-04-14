4f03ce12d4a2e70b61e974dc2f9d90dc761e73bc
sruedam <72003470+sruedam@users.noreply.github.com>
2021-04-13 23:59:49 -0500

    Por favor

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -41,7 +41,9 @@ public class Animal {
 	}
 	
 	public String toString() {
-		return "Provicional";
+		return "Mi nombre es "+ this.nombre +", tengo una edad de "+ this.edad +", habito en "
+				+ this.habitat +" y mi genero es "+ this.genero +", la zona en la que me ubico es "
+				+ this.zona +", en el "+ this.zona.getZoo();
 	}
 	
 	public void setZona(Zona zona) {
